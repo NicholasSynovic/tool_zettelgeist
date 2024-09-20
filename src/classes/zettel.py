3fb24931b5ad186a3f9d3c27b1459712e96fa9a9
Nicholas M. Synovic <nicholas.synovic@gmail.com>
2024-09-20 18:30:36 -0500

    Use pydantic BaseModels to power classes

--- src/classes/zettel.py
+++ src/classes/zettel.py
@@ -1,50 +1,32 @@
 from pathlib import Path
 from typing import List
 
-
-class CiteField:
-    def __init__(self, bibkey: str = "", page: int = -1) -> None:
-        self.bibkey: str = bibkey
-        self.page: int = page
+from pydantic import BaseModel
 
 
-class DateField:
-    def __init__(self, year: int = -1, era: str = "AD") -> None:
-        self.year: int = year
-        self.era: str = era
+class CiteField(BaseModel):
+    bibkey: str = ""
+    page: int = -1
 
 
-class Zettel:
-    def __init__(
-        self,
-        title: str = "",
-        bibkey: str = "",
-        bibtex: str = "",
-        ris: str = "",
-        inline: str = "",
-        url: str = "",
-        summary: str = "",
-        comment: str = "",
-        note: str = "",
-        tags: List[str] = [""],
-        mentions: List[str] = [""],
-        cite: CiteField = CiteField(),
-        dates: List[DateField] = [DateField()],
-        filename: Path = Path(),
-        document: str = "",
-    ) -> None:
-        self.title: str = title
-        self.bibkey: str = bibkey
-        self.bibtex: str = bibtex
-        self.ris: str = ris
-        self.inline: str = inline
-        self.url: str = url
-        self.summary: str = summary
-        self.comment: str = comment
-        self.note: str = note
-        self.document: str = document
-        self.filename: Path = filename.resolve(strict=True)
-        self.tags: List[str] = tags
-        self.mentions: List[str] = mentions
-        self.cite: CiteField = cite
-        self.dates: List[DateField] = dates
+class DateField(BaseModel):
+    year: int = -1
+    era: str = "AD"
+
+
+class Zettel(BaseModel):
+    title: str = ""
+    bibkey: str = ""
+    bibtex: str = ""
+    ris: str = ""
+    inline: str = ""
+    url: str = ""
+    summary: str = ""
+    comment: str = ""
+    note: str = ""
+    tags: List[str] = [""]
+    mentions: List[str] = [""]
+    cite: CiteField = CiteField()
+    dates: List[DateField] = [DateField()]
+    filename: Path = Path()
+    document: str = ""
